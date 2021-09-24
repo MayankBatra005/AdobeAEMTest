100c55635b6c73091b86ec27c7276bf4798dd70e
MayankBatra005 <er.mayank005@gmail.com>
2021-09-23 20:14:08 -0700

    Intial Commit.

---
+++ aem/src/main/java/com/adobe/aem/models/Conversions.java
@@ -0,0 +1,13 @@
+package com.adobe.aem.models;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Conversions {
+    String input;
+    String output;
+}
---
+++ aem/src/main/java/com/adobe/aem/service/RomanConvertorService.java
@@ -0,0 +1,7 @@
+package com.adobe.aem.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RomanConvertorService {
+}
