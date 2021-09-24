1c40843e1848e60b125b06a077136ad6d97a7dc6
MayankBatra005 <er.mayank005@gmail.com>
2021-09-23 23:02:22 -0700

    Extension 2 implementation in progress.

--- aem/src/main/java/com/adobe/aem/controllers/RomanNumeral.java
+++ aem/src/main/java/com/adobe/aem/controllers/RomanNumeral.java
@@ -3,18 +3,36 @@ package com.adobe.aem.controllers;
 import com.adobe.aem.models.Conversions;
 import com.adobe.aem.service.RomanConvertorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
+@Validated
+@RequestMapping("/romannumeral")
 public class RomanNumeral {
 
     @Autowired
     RomanConvertorService romanConvertorService;
 
-    @GetMapping("/romannumeral")
-    public Conversions ToRoman(@RequestParam(name="integer") Integer integer){
-        return romanConvertorService.ToRoman(integer);
+    @GetMapping(params = {"integer"})
+    public Conversions ToRoman( @RequestParam(name="integer") int integer) throws Exception {
+        if((integer<1)||(integer>255))
+            throw new Exception("{integer} must be any integer value in the range 1-255");
+        return romanConvertorService.toRoman(integer);
+    }
+
+    @GetMapping(params = {"min","max"})
+    public List<Conversions> convertIntToRomanRange(@RequestParam(required =true ,name="min") Integer min,
+                                                    @RequestParam(required =true ,name="max") Integer max)throws Exception {
+        if((min<1)||(max>3999)||(min>max))
+            throw new Exception("{integer} must be an integer value for both the min and max parameters. Both\n" +
+                    "min and max must be provided. Min must be less than max. Both must be in the\n" +
+                    "supported range of 1 - 3999");
+
+        return romanConvertorService.toRomanParallel(min,max);
     }
 }
